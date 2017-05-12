c19e83921607cd8b5ee1da1bc158693306d50081
Dan McPherson <dmcphers@redhat.com>
2017-05-12 17:41:46 -0400

    Add a test and fix some terminology

--- pman_openshift.py
+++ pman_openshift.py
@@ -18,18 +18,18 @@ class OpenShiftManager(object):
         parser = ArgumentParser(description='Manage a OpenShift cluster')
 
         group = parser.add_mutually_exclusive_group()
-        group.add_argument("-s", "--schedule", help="schedule a new service",
+        group.add_argument("-s", "--schedule", help="schedule a new job",
                            metavar='name')
-        group.add_argument("-r", "--remove", help="remove a previously scheduled service",
+        group.add_argument("-r", "--remove", help="remove a previously scheduled job",
                            metavar='name')
-        group.add_argument("--state", help="print state of scheduled service",
+        group.add_argument("--state", help="print state of scheduled job",
                            metavar='name')
         parser.add_argument("--conffile", help="OpenShift cluster configuration file")
         parser.add_argument("-p", "--project", help="The OpenShift project to create jobs in")
         parser.add_argument("-i", "--image",
-                            help="docker image for the scheduled service container")
+                            help="docker image for the scheduled job container")
         parser.add_argument("-c", "--command",
-                            help="command to be run inside scheduled service container")
+                            help="command to be run inside scheduled job container")
         parser.add_argument("-m", "--mount", help="mount directory in the cluster",
                             metavar='dir')
         self.parser = parser
@@ -71,10 +71,9 @@ spec:
         job_yaml = yaml.load(job)
         resp = self.kube_v1_batch_client.create_namespaced_job(namespace=project, body=job_yaml)
 
-
     def get_job(self, name, project):
         """
-        Get docker container for a previously scheduled service object.
+        Get the previously scheduled job object.
         """
         return self.kube_v1_batch_client.read_namespaced_job(name, project)
 
